c8fbedcc508078ecf8c0c68e1cf34ac3b22c7dfa
unknown <jason.wong6507@gmail.com>
2022-08-29 20:28:31 -0400

    Added the dogcontent and dog image using the index,js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let content = document.querySelector('.js-generated.content')
+/*let content = document.querySelector('.js-generated.content')
 
 let header = document.createElement('h1')
 header.setAttribute('class', 'dog-name')
@@ -18,5 +18,22 @@ content.append(dogImage)
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
 content.append(dogDetails)
+*/
+let content = document.querySelector('.js-generated.content')
 
+let header = document.createElement('h1')
+header.setAttribute('class','dog-name')
+header.textContent= 'Rizzo'
 
+//This would have be the append('Rizzo)
+content.append(header)
+
+let dogContent = document.createElement('div')
+dogContent.setAttribute('class', 'dog-content')
+content.append(dogContent)
+
+//You append dogImage into dogContent that way it is in the div
+let dogImage = document.createElement('img')
+dogImage.setAttribute('class', 'dog-image')
+dogImage.setAttribute('src', './assets/rizzo.jpg')
+dogContent.append(dogImage)
